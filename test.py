7f68bb6b253ce1abb65cb16cd43b5b0c0d227978
vipinkumar2 <110365221+vipinkumar2@users.noreply.github.com>
2022-10-26 23:56:24 -0400

    dacd

--- test.py
+++ test.py
@@ -1,15 +1,49 @@
 import pyautogui, random
 
-from utils import EMAIL_PASSWORD, fake_name, random_sleep
+from utils import BACKUP_EMAIL, EMAIL_PASSWORD, fake_name, get_sms, random_sleep
 random_sleep(7,9)
 # name, fname, lname = fake_name()
 # username = str(fname)+str(lname)+str(random.randint(10000,99999))
 # print(username)
 # pyautogui.typewrite(str(fname)+'\t'+str(lname)+'\t'+username+'\t\t'+EMAIL_PASSWORD+'\t'+EMAIL_PASSWORD+'\t\t\n')
-new_number = '601170036429'
-pyautogui.typewrite(f'\t\t\t\t\t\t+{new_number}\t\n')
+# new_number = '+601139009235'
+# # # pyautogui.typewrite(f'\t\t\t\t\t\t+{new_number}\t\n')
+# for i in range(5):
+#     pyautogui.typewrite('\t')
+#     pyautogui.sleep(3)
+# pyautogui.typewrite(f'+{new_number}\t\n')
 
-    
+# number_otp = input('OTP :')
+# random_sleep()
+# pyautogui.typewrite(str(number_otp)+'\t\n')
+# pyautogui.press('enter')
+
+
+
+random_sleep()
+# month_first_li =  ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+# BIRTHDATE = str(random.randint(2,28))
+# BIRTH_MONTH = random.choice(month_first_li)
+# BIRTH_YEAR = str(random.randint(1985,2003))
+# pyautogui.typewrite('\t\t\t\t\t\t\t\t\t') 
+# for _ in range(9):
+#     pyautogui.typewrite('\t')
+#     pyautogui.sleep(3)
+# pyautogui.typewrite('\n')
+
+
+pyautogui.press('f6')
+pyautogui.typewrite('https://www.youtube.com\n')
+random_sleep()
+
+pyautogui.press('f5')
+# pyautogui.typewrite(BACKUP_EMAIL+'\t')
+# pyautogui.typewrite(BIRTHDATE+'\t')
+# pyautogui.typewrite(BIRTH_MONTH+'\t')
+# pyautogui.typewrite(BIRTH_YEAR+'\t')
+# pyautogui.typewrite('r\t\t\n')
+
 # month_first_li =  ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 # BIRTHDATE = str(random.randint(2,28))
@@ -17,7 +51,8 @@ pyautogui.typewrite(f'\t\t\t\t\t\t+{new_number}\t\n')
 # BIRTH_YEAR = str(random.randint(1985,2003))
 # pyautogui.typewrite('\t\t\t\t\t\t\t') 
 # pyautogui.typewrite(BACKUP_EMAIL+'\t')
-# pyautogui.typewrite(BIRTHDATE+'\t')
+# pyautogui.typewrite(BIRTHDATE+'\t')                       +601137967915   
+
 # pyautogui.typewrite(BIRTH_MONTH+'\t')
 # pyautogui.typewrite(BIRTH_YEAR+'\t')
 # pyautogui.typewrite('r\t\t\n')
